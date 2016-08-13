e7bb732ad59704ed9f4c47130a5f251ec1bcd3d6
viparthasarathy <viparthasarathy@gmail.com>
2016-08-13 14:22:55 -0700

    Add views/registration.html with form for signup

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
-})
+});
 
 
 app.listen(3000, function() {
-  console.log("Now listening to http://localhost:3000.");
-}
+  console.log("Now listening to http://localhost:3000");
+});
