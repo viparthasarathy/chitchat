2384dae2b8d17b978e089580994cd36e8b1f48e4
viparthasarathy <viparthasarathy@gmail.com>
2016-08-13 14:38:42 -0700

    Add get '/signup' route

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+// modules
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+// configuration
+
+const port = process.env.PORT || 3000;
+
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/views/registration.html');
+})
+
+app.post('/signup', (req, res) => {
+
+})
 
 
 app.listen(3000, function() {
