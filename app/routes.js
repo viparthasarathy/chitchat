665392eb62caebbec5ccdf69d7514801f1bbf61d
viparthasarathy <viparthasarathy@gmail.com>
2016-08-13 15:31:50 -0700

    Add new node modules: morgan, passport, jwt, bcrypt

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,13 @@
 const User = require('./models/user');
 
 module.exports = function(app) {
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
-  });
-
-  app.get('/signup', (req, res) => {
-    res.sendFile(__dirname + '/views/registration.html');
-  });
 
   app.post('/signup', (req, res) => {
 
-  });  
-}
+  });
+
+  app.get('*', (req, res) => {
+    res.sendfile('./public/views/index.html');
+  });
+
+};
